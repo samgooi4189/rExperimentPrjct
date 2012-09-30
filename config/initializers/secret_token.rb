db15c387951199706a71b025443183257b361e9f
samgooi4189 <samgooi4189@samgooi4189-VirtualBox.(none)>
2012-09-30 15:57:01 -0500

    Lets Go

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstwebdeploy::Application.config.secret_token = '397ce10727a2a4c500530eceec560902d0e56ff36308536c94975e0e24f03aee46d304802364a6387a0dd0117c367b30ec3ae26ffb568901621d6ce43c78e0b4'
